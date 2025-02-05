548e07aefea713fef926b6c906ecc80555c41aaa
Kauan B. Brito <125837412+BoaventuraK@users.noreply.github.com>
2025-02-05 17:26:07 -0300

    .

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route("/")
-@app.route("/index")
-def index():
-    nome = "Kauan"
-    return render_template('index.html', nome=nome)
+@app.route("/index/", defaults={"nome":"User", "idade":"sua idade"})
+@app.route("/index/<nome>/<idade>")
+def index(nome, idade):
+    return render_template('index.html', nome=nome, idade=idade)
 
 @app.route("/contato")
 def contato():
